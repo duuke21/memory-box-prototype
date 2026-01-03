9dbb08b8da91864c144a3794e124a5e71d71af26
duuke21 <110412609+duuke21@users.noreply.github.com>
2026-01-03 11:34:23 -0500

    Create search.js

---
+++ pages/search.js
@@ -0,0 +1,10 @@
+export default function Search() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Search Memories</h1>
+      <input placeholder="Search..." style={{ width: '100%' }} />
+      {/* Add results list later */}
+      <a href="/">Back</a>
+    </div>
+  );
+}
